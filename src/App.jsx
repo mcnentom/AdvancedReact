f008838dc7741e86ddbbbdedfb42da21012a6083
mcnentom <tommymcnen016@gmail.com>
2024-02-13 23:07:01 +0300

    my commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { Counter } from './features/Counter'
+
+function App() {
+  return (
+    <>
+     <Counter/>
+    </>
+  )
+}
+
+export default App
